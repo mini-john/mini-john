d2e374189d6835d17505d333384a03b148edc611
jonat <jonat@LAPTOP-QS6L9PMD>
2022-07-28 17:26:42 +0200

    commit netbeans 3

--- Library/src/test/java/com/jkalvered/library/date/TestDate.java
+++ Library/src/test/java/com/jkalvered/library/date/TestDate.java
@@ -31,7 +31,6 @@ public class TestDate {
         JewishDate date = DateNiddah.getDateJewish(dateStr);
         LOGGER.info(date.toString());
         Assert.hasText("7 Adar II, 5776", date.toString());
-         Assert.hasText("7 Adar II, 5776", date.toString());
     }
 
     @Test
